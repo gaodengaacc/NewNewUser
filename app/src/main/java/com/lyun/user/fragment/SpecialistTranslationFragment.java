da9d1d25c32df4f0358ed65bb3f62065317f43ae
zhengchengyu <912517171@qq.com>
2016-12-21 11:08:45 +0800

    “专人翻译”界面UI

--- app/src/main/java/com/lyun/user/fragment/SpecialistTranslationFragment.java
+++ app/src/main/java/com/lyun/user/fragment/SpecialistTranslationFragment.java
@@ -5,10 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lyun.user.R;
 
-public class SpecialistTranslationFragment extends Fragment {
+import butterknife.BindView;
+
+public class SpecialistTranslationFragment extends Fragment implements View.OnClickListener {
+    @BindView(R.id.imageView_change)
+    ImageView imageViewChange;
+    @BindView(R.id.textView_title_change)
+    TextView textViewTitleChange;
+    @BindView(R.id.imageView_way_change)
+    ImageView imageViewWayChange;
+
+    private boolean mCommunicationMode = false;
 
     public SpecialistTranslationFragment() {
         // Required empty public constructor
@@ -32,7 +44,34 @@ public class SpecialistTranslationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specialist_translation, container, false);
+        View view = inflater.inflate(R.layout.fragment_specialist_translation, container, false);
+        imageViewChange = (ImageView) view.findViewById(R.id.imageView_change);
+        imageViewWayChange = (ImageView) view.findViewById(R.id.imageView_way_change);
+        textViewTitleChange = (TextView) view.findViewById(R.id.textView_title_change);
+        imageViewChange.setOnClickListener(this);
+        imageViewWayChange.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageView_change:
+                if (!mCommunicationMode) {
+                    imageViewChange.setImageResource(R.mipmap.call_phone);
+                    imageViewWayChange.setImageResource(R.drawable.image_call_selector);
+                    textViewTitleChange.setText("语音呼叫");
+                    mCommunicationMode = true;
+                } else {
+                    imageViewChange.setImageResource(R.mipmap.call_picture);
+                    imageViewWayChange.setImageResource(R.mipmap.picture_dial_normal);
+                    textViewTitleChange.setText("图文翻译");
+                    mCommunicationMode = false;
+                }
+                break;
+            case R.id.imageView_way_change:
+//                ToastUtil.show(getActivity(),"点击");
+                break;
+        }
+    }
 }
