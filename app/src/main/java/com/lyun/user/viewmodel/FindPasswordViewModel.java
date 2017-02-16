5e79980e3e82d840341c900e5d923b44de899e6d
zhengchengyu <912517171@qq.com>
2017-02-16 20:02:15 +0800

    部分功能

--- app/src/main/java/com/lyun/user/viewmodel/FindPasswordViewModel.java
+++ app/src/main/java/com/lyun/user/viewmodel/FindPasswordViewModel.java
@@ -1,9 +1,14 @@
 package com.lyun.user.viewmodel;
 
 import android.databinding.BaseObservable;
+import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
+import android.os.CountDownTimer;
 
 import com.lyun.library.mvvm.command.RelayCommand;
 import com.lyun.library.mvvm.viewmodel.ViewModel;
+import com.lyun.user.R;
+import com.lyun.utils.Validator;
 
 import net.funol.databinding.watchdog.annotations.WatchThis;
 
@@ -12,11 +17,72 @@ import net.funol.databinding.watchdog.annotations.WatchThis;
  */
 
 public class FindPasswordViewModel extends ViewModel {
-
+    public final ObservableField<String> username = new ObservableField<>("");//手机号码
+    public final ObservableField<String> smscode = new ObservableField<>("");//获取的验证码
+    public final ObservableField<String> newPassword = new ObservableField<>("");//新密码
+    public final ObservableField<String> mSendSmsCode = new ObservableField<>("");//获取验证码倒计时
+    public final ObservableInt isVisible = new ObservableInt();
+    public final ObservableField<Boolean> clickable = new ObservableField<>();
+    private TimeCount timeCount = new TimeCount(60000, 1000);
+    private Boolean aBoolean = false;
     @WatchThis
     public final BaseObservable onFindPasswordSuccess = new BaseObservable();
+    @WatchThis
+    public final BaseObservable onNumberBlank = new BaseObservable();
+    @WatchThis
+    public final BaseObservable onNumberWrong = new BaseObservable();
+
+    public FindPasswordViewModel() {
+        mSendSmsCode.set("获取验证码");
+        clickable.set(true);
+        isVisible.set(R.mipmap.ic_password_invisible);
+    }
 
     public RelayCommand onSubmitClick = new RelayCommand(() -> {
         onFindPasswordSuccess.notifyChange();
     });
+    public RelayCommand onVisible = new RelayCommand(() -> {
+        if (!aBoolean) {
+            isVisible.set(R.mipmap.ic_password_visible);
+            aBoolean = true;
+        } else {
+            isVisible.set(R.mipmap.ic_password_invisible);
+            aBoolean = false;
+        }
+
+    });
+    public RelayCommand onGetSMSCodeButtonClick = new RelayCommand(() -> {
+        if (("".equals(username.get()) || (username.get() == null))) {
+            onNumberBlank.notifyChange();
+        } else if (!Validator.isMobileNO(username.get())) {
+            onNumberWrong.notifyChange();
+        } else {
+            timeCount.start();
+        }
+    });
+
+    class TimeCount extends CountDownTimer {
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            mSendSmsCode.set(millisUntilFinished / 1000 + "S");
+            clickable.set(false);
+        }
+
+        @Override
+        public void onFinish() {
+            mSendSmsCode.set("获取验证码");
+            clickable.set(true);
+        }
+    }
 }
--- app/src/main/java/com/lyun/user/viewmodel/RegisterVerifyPhoneViewModel.java
+++ app/src/main/java/com/lyun/user/viewmodel/RegisterVerifyPhoneViewModel.java
@@ -2,6 +2,7 @@ package com.lyun.user.viewmodel;
 
 import android.databinding.BaseObservable;
 import android.databinding.ObservableField;
+import android.os.CountDownTimer;
 
 import com.lyun.library.mvvm.command.RelayCommand;
 import com.lyun.library.mvvm.viewmodel.ViewModel;
@@ -19,6 +20,10 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 public class RegisterVerifyPhoneViewModel extends ViewModel {
     public final ObservableField<String> username = new ObservableField<>("");//手机号码
     public final ObservableField<String> smscode = new ObservableField<>("");//获取的验证码
+    public final ObservableField<String> mSendSmsCode = new ObservableField<>("");//获取验证码倒计时
+    public final ObservableField<Boolean> clickable = new ObservableField<>();
+    private TimeCount timeCount = new TimeCount(60000, 1000);
+
     @WatchThis
     public final BaseObservable onVerifySuccess = new BaseObservable();
     @WatchThis
@@ -26,12 +31,18 @@ public class RegisterVerifyPhoneViewModel extends ViewModel {
     @WatchThis
     public final BaseObservable onNumberWrong = new BaseObservable();
 
+    public RegisterVerifyPhoneViewModel() {
+        mSendSmsCode.set("获取验证码");
+        clickable.set(true);
+    }
+
     public RelayCommand onGetSMSCodeButtonClick = new RelayCommand(() -> {
         if (("".equals(username.get()) || (username.get() == null))) {
             onNumberBlank.notifyChange();
         } else if (!Validator.isMobileNO(username.get())) {
             onNumberWrong.notifyChange();
         } else {
+            timeCount.start();
             getSmsCode(username.get());
         }
     });
@@ -45,5 +56,31 @@ public class RegisterVerifyPhoneViewModel extends ViewModel {
         new RegisterVerifyPhoneModel().getSmsCode(username)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe();
+    }
+
+    class TimeCount extends CountDownTimer {
+
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            mSendSmsCode.set(millisUntilFinished / 1000 + "S");
+            clickable.set(false);
+        }
+
+        @Override
+        public void onFinish() {
+            mSendSmsCode.set("获取验证码");
+            clickable.set(true);
+        }
     }
 }
