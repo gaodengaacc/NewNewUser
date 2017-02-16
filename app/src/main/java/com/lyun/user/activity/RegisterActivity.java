3faaad1e0eedc8038c08008566017846de4ca7a6
赵尉尉 <4332weizi@gmail.com>
2017-02-16 09:58:59 +0800

    优化Register

--- app/src/main/java/com/lyun/user/activity/RegisterActivity.java
+++ app/src/main/java/com/lyun/user/activity/RegisterActivity.java
@@ -1,14 +1,18 @@
 package com.lyun.user.activity;
 
+import android.databinding.BaseObservable;
+import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
+import android.widget.Toast;
 
 import com.lyun.library.mvvm.view.activity.GeneralToolbarActivity;
 import com.lyun.library.mvvm.viewmodel.GeneralToolbarViewModel;
 import com.lyun.user.R;
 import com.lyun.user.databinding.ActivityRegisterBinding;
 import com.lyun.user.viewmodel.RegisterViewModel;
+import com.lyun.user.viewmodel.watchdog.IRegisterViewModelCallbacks;
 
-public class RegisterActivity extends GeneralToolbarActivity<ActivityRegisterBinding, RegisterViewModel> {
+public class RegisterActivity extends GeneralToolbarActivity<ActivityRegisterBinding, RegisterViewModel> implements IRegisterViewModelCallbacks {
 
     @Override
     protected int getBodyLayoutId() {
@@ -29,5 +33,15 @@ public class RegisterActivity extends GeneralToolbarActivity<ActivityRegisterBin
     protected RegisterViewModel createBodyViewModel() {
         return new RegisterViewModel()
                 .setPropertyChangeListener(this);
+    }
+
+    @Override
+    public void onRegisterSuccess(BaseObservable observableField, int fieldId) {
+        finish();
+    }
+
+    @Override
+    public void onRegisterFailed(ObservableField<Throwable> observableField, int fieldId) {
+        Toast.makeText(this, observableField.get().getMessage(), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/lyun/user/viewmodel/RegisterViewModel.java
+++ app/src/main/java/com/lyun/user/viewmodel/RegisterViewModel.java
@@ -1,21 +1,32 @@
 package com.lyun.user.viewmodel;
 
+import android.databinding.BaseObservable;
 import android.databinding.ObservableField;
 
+import com.lyun.api.response.APIResult;
 import com.lyun.library.mvvm.command.RelayCommand;
 import com.lyun.library.mvvm.viewmodel.ViewModel;
 import com.lyun.user.model.RegisterModel;
 
+import net.funol.databinding.watchdog.annotations.WatchThis;
+
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by ZHAOWEIWEI on 2017/1/16.
  */
 
 public class RegisterViewModel extends ViewModel {
+
     public final ObservableField<String> username = new ObservableField<>("");
     public final ObservableField<String> password = new ObservableField<>("");
     public final ObservableField<String> comfirmPassword = new ObservableField<>("");
+
+    @WatchThis
+    public final BaseObservable onRegisterSuccess = new BaseObservable();
+    @WatchThis
+    public final ObservableField<Throwable> onRegisterFailed = new ObservableField<>();
 
     public RelayCommand onRegisterButtonClick = new RelayCommand(() -> {
 //        if ("".equals(username.get())){
@@ -25,12 +36,12 @@ public class RegisterViewModel extends ViewModel {
 //        }else{
 //        }
         register(username.get(), password.get());
-        getActivity().finish();
     });
 
     private void register(String username, String password) {
         new RegisterModel().register(username, password)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe();
+                .subscribe(apiResult -> onRegisterSuccess.notifyChange(),
+                        throwable -> onRegisterFailed.set(throwable));
     }
 }
