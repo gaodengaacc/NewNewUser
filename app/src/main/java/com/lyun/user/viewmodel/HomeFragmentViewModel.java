be0c3312001ef4a26c0f116bf64b38a3cd62f9bf
赵尉尉 <4332weizi@gmail.com>
2017-02-23 17:25:56 +0800

    更换cache存储，LanguagePicker更新

--- app/src/main/java/com/lyun/user/viewmodel/HomeFragmentViewModel.java
+++ app/src/main/java/com/lyun/user/viewmodel/HomeFragmentViewModel.java
@@ -1,19 +1,27 @@
 package com.lyun.user.viewmodel;
 
+import android.databinding.BaseObservable;
 import android.databinding.ObservableField;
 import android.databinding.ObservableInt;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.View;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.lyun.library.mvvm.command.RelayCommand;
+import com.lyun.library.mvvm.command.consumer.Consumer0;
 import com.lyun.library.mvvm.observable.util.ObservableNotifier;
 import com.lyun.library.mvvm.viewmodel.ViewModel;
 import com.lyun.user.Account;
+import com.lyun.user.AppApplication;
+import com.lyun.user.Constants;
 import com.lyun.user.R;
+import com.lyun.user.api.response.FindLanguageResponse;
 import com.lyun.user.model.RemainingTimeModel;
 import com.lyun.user.model.TranslationOrderModel;
 import com.lyun.user.model.TranslationOrderModel.OrderType;
+import com.lyun.utils.ACache;
 import com.lyun.utils.filecache.Cache;
 import com.lyun.utils.filecache.CacheUtil;
 
@@ -36,19 +44,17 @@ public class HomeFragmentViewModel extends ViewModel {
 
     public final ObservableInt textViewColor1 = new ObservableInt();//语音呼叫
     public final ObservableInt textViewColor2 = new ObservableInt();//图文翻译
-    public final ObservableField<String> textViewTargetLanguage = new ObservableField<>();//目标语言
+    public final ObservableField<FindLanguageResponse> mCurrentLanguage = new ObservableField<>();//目标语言
     public final ObservableField<Typeface> typeface1 = new ObservableField<>();//设置字体加粗
     public final ObservableField<Typeface> typeface2 = new ObservableField<>();
     public final ObservableField<String> unUserTime = new ObservableField<>();//剩余时间
 
-    private List list;
-
+    @WatchThis
+    public final BaseObservable onPickLanguage = new BaseObservable();
     @WatchThis
     public final ObservableField<OrderType> onTranslationOrderGenerated = new ObservableField<>();
     @WatchThis
     public final ObservableField<String> onTranslationOrderGenerateFail = new ObservableField<>();
-
-    LanguagePickerDialogViewModel languagePickerDialogViewModel;
 
     public HomeFragmentViewModel() {
         initData();
@@ -76,28 +82,19 @@ public class HomeFragmentViewModel extends ViewModel {
         textViewColor2.set(Color.parseColor("#333333"));
         typeface1.set(Typeface.defaultFromStyle(Typeface.BOLD));
         typeface2.set(Typeface.defaultFromStyle(Typeface.NORMAL));
-        textViewTargetLanguage.set("英语");
     }
 
     public RelayCommand onRequestTranslation = new RelayCommand(() -> {
         // 0=图文 1=语音
-        new TranslationOrderModel().generateOrder(textViewTargetLanguage.get(), mTranslationOrderType.getValue())
+        new TranslationOrderModel().generateOrder(mCurrentLanguage.get().getId() + "", mTranslationOrderType.getValue())
                 .subscribe(orderId -> ObservableNotifier.alwaysNotify(onTranslationOrderGenerated, mTranslationOrderType),
                         throwable -> ObservableNotifier.alwaysNotify(onTranslationOrderGenerateFail, throwable.getMessage()));
     });
 
-    public void languagePickerLinearLayoutClick(View view) {//选取目标语言
-        if (languagePickerDialogViewModel == null) {
-            CacheUtil.getInstance().getData(Cache.DATA_TYPE_FIND_BY_LANGUAGE, result -> {
-                if (result != null)
-                    list = (List) result;
-            });
-            languagePickerDialogViewModel = new LanguagePickerDialogViewModel(view.getContext(), list);
-        }
-
-        languagePickerDialogViewModel.setOnLanguagePickedListener(language -> textViewTargetLanguage.set(language));
-        languagePickerDialogViewModel.show();
-    }
+    //选取目标语言
+    public RelayCommand onPickLanguageClickCommand = new RelayCommand(() -> {
+        onPickLanguage.notifyChange();
+    });
 
     public void modelChangeImageViewClick(View view) {//选择翻译模式，语言或者图文
         if (mTranslationOrderType == OrderType.MESSAGE) {
