b74ea648e6964dfa10dffd78d646ad28bde3d59e
赵尉尉 <4332weizi@gmail.com>
2017-03-07 19:40:08 +0800

    更新通话开始和通话结束接口

--- app/src/main/java/com/lyun/user/api/request/TranslatorStatusBean.java
+++ app/src/main/java/com/lyun/user/api/request/TranslatorStatusBean.java
@@ -8,13 +8,26 @@ import com.lyun.api.request.BaseRequest;
  * do()
  */
 
-public class TranslatorStatusBean extends BaseRequest{
+public class TranslatorStatusBean extends BaseRequest {
+
+    // 用户类型(0客户 1翻译)
+    private String userType = "0";
     private String userOrderid;
     private String phoneState;//通话标记 0 =开始通话， 1= 会话结束
-    public TranslatorStatusBean(String userOrderid,String phoneState){
+
+    public TranslatorStatusBean(String userOrderid, String phoneState) {
         this.userOrderid = userOrderid;
         this.phoneState = phoneState;
     }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
     public String getUserOrderid() {
         return userOrderid;
     }
