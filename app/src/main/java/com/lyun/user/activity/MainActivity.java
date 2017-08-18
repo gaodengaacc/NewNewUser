2e035be6ac0e1c3d9b442a802721126718f6efb0
赵尉尉 <4332weizi@gmail.com>
2017-08-18 12:21:29 +0800

    调整BaseActivity

--- app/src/main/java/com/lyun/user/activity/MainActivity.java
+++ app/src/main/java/com/lyun/user/activity/MainActivity.java
@@ -1,17 +1,14 @@
 package com.lyun.user.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.view.KeyEvent;
-import android.view.Window;
 
 import com.lyun.library.mvvm.observable.util.ObservableNotifier;
 import com.lyun.library.mvvm.view.activity.MvvmActivity;
-import com.lyun.library.mvvm.viewmodel.ViewModel;
 import com.lyun.user.R;
 import com.lyun.user.adapter.MainPagerAdapter;
 import com.lyun.user.databinding.ActivityMainBinding;
@@ -23,8 +20,6 @@ import com.lyun.user.im.config.preference.UserPreferences;
 import com.lyun.user.viewmodel.MainActivityViewModel;
 import com.netease.nimlib.sdk.NIMClient;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +28,7 @@ public class MainActivity extends MvvmActivity<ActivityMainBinding, MainActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setStatusBarDarkMode(true, this);
+        setStatusBarDarkMode(true);
     }
 
     @NonNull
@@ -83,17 +78,4 @@ public class MainActivity extends MvvmActivity<ActivityMainBinding, MainActivity
         return new MainPagerAdapter(this, getSupportFragmentManager(), fragments);
     }
 
-    public void setStatusBarDarkMode(boolean darkmode, Activity activity) {
-        Class<? extends Window> clazz = activity.getWindow().getClass();
-        try {
-            int darkModeFlag = 0;
-            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
-            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
-            darkModeFlag = field.getInt(layoutParams);
-            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
-            extraFlagField.invoke(activity.getWindow(), darkmode ? darkModeFlag : 0, darkModeFlag);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
--- library/src/main/java/com/lyun/activity/BaseActivity.java
+++ library/src/main/java/com/lyun/activity/BaseActivity.java
@@ -1,14 +1,16 @@
 package com.lyun.activity;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.view.Window;
 
 import com.lyun.BaseApplication;
-import com.lyun.widget.dialog.ProgressBarDialog;
 import com.squareup.leakcanary.RefWatcher;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 
 /**
@@ -31,6 +33,24 @@ public class BaseActivity extends AppCompatActivity {
         // LeakCanary
         RefWatcher refWatcher = BaseApplication.getRefWatcher();
         refWatcher.watch(this);
+    }
+
+    public void setStatusBarDarkMode(boolean darkmode) {
+        setMiuiStatusBarDarkMode(darkmode, this);
+    }
+
+    public void setMiuiStatusBarDarkMode(boolean darkmode, Activity activity) {
+        Class<? extends Window> clazz = activity.getWindow().getClass();
+        try {
+            int darkModeFlag = 0;
+            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(activity.getWindow(), darkmode ? darkModeFlag : 0, darkModeFlag);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
