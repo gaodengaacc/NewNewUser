8f2ff8d3538f6670f5ba5660143f8f826cee8ff8
gaodeng <243316807@qq.com>
2017-09-08 14:36:55 +0800

    修改三方登录快速点击

--- app/src/main/java/com/lyun/user/activity/LoginActivity.java
+++ app/src/main/java/com/lyun/user/activity/LoginActivity.java
@@ -102,6 +102,7 @@ public class LoginActivity extends MvvmActivity<ActivityLoginBinding, LoginViewM
         super.onPause();
         if (dialogViewModel != null)
             dialogViewModel.dismiss();
+        getActivityViewModel().clickFlag = false;
     }
 
     public void init() {
@@ -336,7 +337,7 @@ public class LoginActivity extends MvvmActivity<ActivityLoginBinding, LoginViewM
 
         @Override
         public void onCancel() {
-            Toast.makeText(getBaseContext(), "登录取消", Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "登录失败", Toast.LENGTH_LONG).show();
         }
     };
     WbAuthListener wbListener = new WbAuthListener() {
