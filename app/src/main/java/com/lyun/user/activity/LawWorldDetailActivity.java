6d72c32277bd92e696c0d248bfa3ce5a3ebce744
赵尉尉 <4332weizi@gmail.com>
2017-09-19 16:20:13 +0800

    修复律世界界面相关问题

--- app/src/main/java/com/lyun/user/activity/LawWorldDetailActivity.java
+++ app/src/main/java/com/lyun/user/activity/LawWorldDetailActivity.java
@@ -2,6 +2,7 @@ package com.lyun.user.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 
 import com.lyun.library.mvvm.view.activity.MvvmActivity;
@@ -10,9 +11,11 @@ import com.lyun.user.R;
 import com.lyun.user.api.response.LawWorldResponse;
 import com.lyun.user.databinding.ActivityLawWorldDetailBinding;
 import com.lyun.user.viewmodel.LawWorldDetailViewModel;
+import com.lyun.user.viewmodel.watchdog.ILawWorldDetailViewModelCallbacks;
 
 
-public class LawWorldDetailActivity extends MvvmActivity<ActivityLawWorldDetailBinding, LawWorldDetailViewModel> {
+public class LawWorldDetailActivity extends MvvmActivity<ActivityLawWorldDetailBinding, LawWorldDetailViewModel>
+        implements ILawWorldDetailViewModelCallbacks {
 
     public static final String EXTRA_DATA = "extra_data";
 
@@ -28,11 +31,16 @@ public class LawWorldDetailActivity extends MvvmActivity<ActivityLawWorldDetailB
         LawWorldResponse data = (LawWorldResponse) getIntent().getSerializableExtra(EXTRA_DATA);
         if (data != null)
             data.setUserImg(Constants.IMAGE_BASE_URL + data.getUserImg());
-        return new LawWorldDetailViewModel(data);
+        return new LawWorldDetailViewModel(data).setPropertyChangeListener(this);
     }
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_law_world_detail;
+    }
+
+    @Override
+    public void onTitleBackClick(ObservableField observableField, int fieldId) {
+        finish();
     }
 }
--- app/src/main/java/com/lyun/user/viewmodel/LawWorldDetailViewModel.java
+++ app/src/main/java/com/lyun/user/viewmodel/LawWorldDetailViewModel.java
@@ -4,8 +4,12 @@ import android.databinding.ObservableField;
 import android.databinding.ObservableInt;
 import android.view.View;
 
+import com.lyun.library.mvvm.command.RelayCommand;
+import com.lyun.library.mvvm.observable.util.ObservableNotifier;
 import com.lyun.library.mvvm.viewmodel.ViewModel;
 import com.lyun.user.api.response.LawWorldResponse;
+
+import net.funol.databinding.watchdog.annotations.WatchThis;
 
 import java.util.List;
 
@@ -19,6 +23,9 @@ public class LawWorldDetailViewModel extends ViewModel {
     public final ObservableInt domin2Visibility = new ObservableInt(View.GONE);
     public final ObservableField<String> domin3 = new ObservableField<>();
     public final ObservableInt domin3Visibility = new ObservableInt(View.GONE);
+
+    @WatchThis
+    public final ObservableField onTitleBackClick = new ObservableField();
 
     public LawWorldDetailViewModel(LawWorldResponse data) {
         this.data.set(data);
@@ -39,5 +46,7 @@ public class LawWorldDetailViewModel extends ViewModel {
         }
         this.data.notifyChange();
     }
+
+    public RelayCommand onBackClick = new RelayCommand(o -> ObservableNotifier.alwaysNotify(onTitleBackClick, null));
 
 }
